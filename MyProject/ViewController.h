6087cf1281d620d98ff9142f6f64d1a760fdb606
Zakaria <zakaria.dalil1@gmail.com>
2019-07-17 17:04:02 +0800

    Project creation

---
+++ MyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProject
+//
+//  Created by Hinteen on 2019/7/17.
+//  Copyright © 2019 Hinteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyProject
+//
+//  Created by Hinteen on 2019/7/17.
+//  Copyright © 2019 Hinteen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
